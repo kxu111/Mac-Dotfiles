ccc73f68cd941b365adaf28898e88881fa8a3b99
kxu111 <potatochipyum5@gmail.com>
2026-03-14 00:48:37 +1100

    rebound caps to escape

--- nix/modules/system.nix
+++ nix/modules/system.nix
@@ -1,41 +1,49 @@
 { self, pkgs, ... }: {
 
-  system.primaryUser = "kenny";
+  system = {
+    primaryUser = "kenny";
 
-  system.defaults = {
-    dock = {
-      autohide = true; 
-      autohide-delay = 0.0;
-      autohide-time-modifier = 0.0;
-      orientation = "left";
-      show-recents = false;
-      minimize-to-application = true;
-      persistent-apps = [
-        "/Applications/Librewolf.app"
-        "/Applications/Vesktop.app"
-	"/Applications/WhatsApp.app"
-      ];
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
     };
-    finder = {
-      FXPreferredViewStyle = "clmv";
-      ShowPathbar = true;
-      AppleShowAllFiles = true;
-      NewWindowTarget = "Documents";
-      QuitMenuItem = true;
-    };
-    loginwindow.GuestEnabled = false;
-    controlcenter.BatteryShowPercentage = true;
-    NSGlobalDomain = {
-      AppleInterfaceStyle = "Dark";
-      KeyRepeat = 2;
-      InitialKeyRepeat = 15;
+
+    defaults = {
+      dock = {
+        autohide = true; 
+        autohide-delay = 0.0;
+        autohide-time-modifier = 0.0;
+        orientation = "left";
+        show-recents = false;
+        minimize-to-application = true;
+        persistent-apps = [
+          "/Applications/Librewolf.app"
+          "/Applications/Vesktop.app"
+  	"/Applications/WhatsApp.app"
+        ];
+        mru-spaces = false;
+      };
+      finder = {
+        FXPreferredViewStyle = "clmv";
+        ShowPathbar = true;
+        AppleShowAllFiles = true;
+        NewWindowTarget = "Documents";
+        QuitMenuItem = true;
+      };
+      loginwindow.GuestEnabled = false;
+      controlcenter.BatteryShowPercentage = true;
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        KeyRepeat = 2;
+        InitialKeyRepeat = 15;
+      };
     };
   };
 
-  nix.settings.experimental-features = "nix-command flakes";
-  system.configurationRevision = self.rev or self.dirtyRev or null;
-  system.stateVersion = 6;
-  nixpkgs.hostPlatform = "aarch64-darwin";
+    nix.settings.experimental-features = "nix-command flakes";
+    system.configurationRevision = self.rev or self.dirtyRev or null;
+    system.stateVersion = 6;
+    nixpkgs.hostPlatform = "aarch64-darwin";
 
 }
 
