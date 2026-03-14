d61899c1b64467fee8d626c480334d6657e0246a
kxu111 <potatochipyum5@gmail.com>
2026-03-15 00:02:45 +1100

    worked on nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,22 +1,49 @@
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
 vim.o.wrap = false
 vim.o.tabstop = 4
 vim.o.swapfile = false
 vim.g.mapleader = " "
+vim.o.winborder = "rounded"
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>cd', ':Ex<CR>')
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format) 
+
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
 vim.pack.add({
-	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/catppuccin/nvim",      name = "catppuccin" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
 })
 
-vim.lsp.enable("lua_ls")
+require "mason".setup()
+require "mini.pick".setup()
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>b', ":Pick buffers<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+
+vim.lsp.enable({ "lua_ls", "nil", "basedpyright" })
+
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
 
 require("catppuccin").setup({
 	flavour = "mocha",
