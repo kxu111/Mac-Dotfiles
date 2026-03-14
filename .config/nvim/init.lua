28665aceff6ab4daaa17af1b97a5f446e07e3041
kxu111 <potatochipyum5@gmail.com>
2026-03-14 19:30:18 +1100

    started on one-liner nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,15 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>cd', ':Ex<CR>')
+
+vim.pack.add({
+		{ src = "https://github.com/catppuccin/nvim" },
+})
