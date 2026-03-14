c9d8408e48deb2fc627f541b61d91f5c5579a2a1
kxu111 <potatochipyum5@gmail.com>
2026-03-15 01:54:23 +1100

    added treesitter & mason

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,23 +7,35 @@ vim.o.swapfile = false
 vim.g.mapleader = " "
 vim.o.winborder = "rounded"
 
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>o', ':update<CR>:source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>vs', ':vs<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>hs', ':sv<CR>')
+
 vim.pack.add({
-	{ src = "https://github.com/catppuccin/nvim",      name = "catppuccin" },
+	{
+		src = "https://github.com/catppuccin/nvim",
+		name = "catppuccin"
+	},
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
 })
 
 require "mason".setup()
 require "mini.pick".setup()
-require "oil".setup()
+require("oil").setup()
+require "nvim-treesitter".install({
+	ensure_installed = { "lua", "nix" },
+	highlight = { enable = true },
+})
 
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>b', ":Pick buffers<CR>")
@@ -31,19 +43,19 @@ vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 
 vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 
-vim.lsp.enable({ "lua_ls", "nil", "basedpyright" })
+vim.lsp.enable({ "lua_ls", "basedpyright", "nil" })
 
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-
-vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
-	end,
+require("conform").setup({
+	formatters_by_ft = {
+		json = { "prettier" },
+		jsonc = { "prettier" },
+		nix = { "alejandra" },
+	},
+	format_on_save = {
+		timeout_ms = 1000,
+		lsp_format = "fallback",
+	},
 })
-vim.cmd("set completeopt+=noselect")
 
 require("catppuccin").setup({
 	flavour = "mocha",
@@ -56,3 +68,17 @@ require("catppuccin").setup({
 	}
 })
 vim.cmd("colorscheme catppuccin-nvim")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
