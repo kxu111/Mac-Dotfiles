182ecacc4d86ef6e4ee0abd4b33872aaa86af172
kxu111 <potatochipyum5@gmail.com>
2026-03-14 21:14:00 +1100

    fixed error

--- nix/modules/system.nix
+++ nix/modules/system.nix
@@ -1,11 +1,9 @@
 { self, ... }: {
 
-  system = {
-    nix.settings.experimental-features = "nix-command flakes";
-    system.configurationRevision = self.rev or self.dirtyRev or null;
-    system.stateVersion = 6;
-    nixpkgs.hostPlatform = "aarch64-darwin";
-  };
+  nix.settings.experimental-features = "nix-command flakes";
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+  system.stateVersion = 6;
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
 }
 
