d81e78a1a1de754a7c3ebf6628d3c395b29ed239
kxu111 <potatochipyum5@gmail.com>
2026-03-14 20:58:32 +1100

    separated homebrew and packages

---
+++ nix/modules/homebrew.nix
@@ -0,0 +1,20 @@
+{ ... }: {
+
+  homebrew = {
+    enable = true;
+    global.autoUpdate = true;
+    onActivation = { cleanup = "zap"; autoUpdate = true; upgrade = true; };
+    brews = [ "mas" ];
+    casks = [
+      "librewolf"
+      "vesktop"
+      "steam"
+      "the-unarchiver"
+    ];
+    masApps = {
+      "Davinci Resolve" = 571213070;
+      "WhatsApp" = 310633997;
+    };
+  };
+
+}
--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }: {
 
   nixpkgs.config.allowUnfree = true;
+
   environment.systemPackages = with pkgs; [ 
-    neovim
+    vim
     btop
     fastfetch
     alacritty
     stow
     ripgrep
+	bob-nvim
   ];
 
   services.yabai.enable = true;
@@ -16,22 +18,5 @@
   fonts.packages = with pkgs; [
     nerd-fonts.iosevka
   ];
-
-  homebrew = {
-    enable = true;
-    global.autoUpdate = true;
-    onActivation = { cleanup = "zap"; autoUpdate = true; upgrade = true; };
-    brews = [ "mas" ];
-    casks = [
-      "librewolf"
-      "vesktop"
-      "steam"
-      "the-unarchiver"
-    ];
-    masApps = {
-      "Davinci Resolve" = 571213070;
-      "WhatsApp" = 310633997;
-    };
-  };
 
 }
