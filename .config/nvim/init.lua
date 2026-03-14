aebc3b33962a1416021a30f326705c21dd32836c
kxu111 <potatochipyum5@gmail.com>
2026-03-14 20:59:55 +1100

    progress on nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,7 +9,23 @@ vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.keymap.set('n', '<leader>cd', ':Ex<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format) 
 
 vim.pack.add({
-		{ src = "https://github.com/catppuccin/nvim" },
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
 })
+
+vim.lsp.enable("lua_ls")
+
+require("catppuccin").setup({
+	flavour = "mocha",
+	color_overrides = {
+		mocha = {
+			base = "#000000",
+			mantle = "#000000",
+			crust = "#000000",
+		}
+	}
+})
+vim.cmd("colorscheme catppuccin-nvim")
