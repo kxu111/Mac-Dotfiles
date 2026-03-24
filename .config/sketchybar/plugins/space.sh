e7e65d28d9ffb8fa2555edb38fa37ab50d57565e
kxu111 <potatochipyum5@gmail.com>
2026-03-24 19:16:46 +1100

    removed sketchybar

--- .config/sketchybar/plugins/space.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Update icon based on selection state
-if [ "$SELECTED" = "true" ]; then
-    sketchybar --set "$NAME" icon="" icon.color=0xffeff1f5
-else
-    sketchybar --set "$NAME" icon="" icon.color=0xffeff1f5
-fi
