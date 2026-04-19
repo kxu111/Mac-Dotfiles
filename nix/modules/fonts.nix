d9a4c2b5c7f4e3f567eb2a46c8c61a1746be0507
kxu111 <potatochipyum5@gmail.com>
2026-04-19 14:06:19 +1000

    new font

--- nix/modules/fonts.nix
+++ nix/modules/fonts.nix
@@ -5,7 +5,7 @@
 }: {
   fonts.packages = [
     pkgs.nerd-fonts.iosevka
-    pkgs.nerd-fonts.jetbrains-mono
     aporetic-nerd-font.packages.${pkgs.stdenv.hostPlatform.system}.default
+	pkgs.maple-mono.NF
   ];
 })
