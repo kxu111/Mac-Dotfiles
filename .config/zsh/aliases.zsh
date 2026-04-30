3a22ab9c09db1a9edfe868797ebed107f5ae0cc5
kxu111 <potatochipyum5@gmail.com>
2026-04-30 18:41:12 +1000

    edit aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,15 +1,20 @@
 alias neofetch='fastfetch'
+
 alias nrs='sudo darwin-rebuild switch --flake ~/nix'
-alias vi='nvim'
 alias reload='source ~/.zshrc'
-alias m='mkdir -p'
-alias cat='bat'
-alias y='yazi'
-alias news='newsboat'
-alias lg='lazygit'
 alias cron-sync="crontab ~/dotfiles/crontab"
 
-alias ls='eza --icons --group-directories-first'
+alias vi='nvim'
+alias y='yazi'
+alias lg='lazygit'
+
+alias news='newsboat'
+alias add-news='~/.config/scripts/add-news.sh'
+
+alias m='mkdir -p'
+alias cat='bat'
+
+alias eza='eza --icons --group-directories-first'
 alias l='eza -l'
 alias ll='eza -l'
 alias la='eza -a'
